256ce1ef59b3a75d9a0ddefc3f8903252e5304a1
shmp-dev <shmp.dev@gmail.com>
2023-11-26 20:06:31 +0900

    [add]ヘッダーを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1>ようこそ</h1>
+      <h1>ルート</h1>
+      <Link href="./articles">記事へ</Link>
     </div>
   );
 }
